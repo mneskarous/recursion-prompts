16d36d100c2d147f859d48749e8f5becfe7c7ed2
Michael Eskarous <mneskarous@gmail.com>
2019-06-23 13:49:48 -0700

    Solved exponent

--- src/recursion.js
+++ src/recursion.js
@@ -88,6 +88,13 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  } else {
+    return 1 / (base * exponent(base, -1 * exp - 1));
+  }
 };
 
 // 8. Determine if a number is a power of two.
