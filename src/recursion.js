c22d329b6f347655e7b103690df7281ede691c5d
Michael Eskarous <mneskarous@gmail.com>
2019-06-23 21:52:49 -0700

    Solved gcd

--- src/recursion.js
+++ src/recursion.js
@@ -188,6 +188,15 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0 || y < 0) {
+    return null;
+  } else if (y % x === 0) {
+    return x;
+  } else if (x > y) {
+    return gcd(y, x);
+  } else {
+    return gcd(x, y % x);
+  }
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
